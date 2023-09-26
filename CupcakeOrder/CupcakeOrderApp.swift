61b38ce0fcb9e34a74339efa387a946729ec9516
Rehman Nahimi <rehman2000@live.com>
2023-09-26 17:15:09 +0100

    Initial Commit

---
+++ CupcakeOrder/CupcakeOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeOrderApp.swift
+//  CupcakeOrder
+//
+//  Created by Ray Nahimi on 26/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
